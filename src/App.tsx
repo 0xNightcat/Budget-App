0f919bd0fbbf264fa43d54d246001d3389a3d177
Behnam <nightcat.sec@gmail.com>
2024-02-18 18:29:30 +0330

    packages installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
+import './App.scss';
 
 function App() {
   return (
     <>
       <h1>Budget App</h1>
+      <h2><i className="bi bi-airplane-engines"></i></h2>
     </>
   )
 }
